74732d1ba83f8a134d3668ada86de2f22fe8a3f6
Benjamin Ng <benjaminngyt@gmail.com>
2021-07-30 10:59:22 +0800

    After creating test

---
+++ tests/test_lib.py
@@ -0,0 +1,4 @@
+from yourpackage.lib import try_me
+
+def test_length_of_try_me():
+    assert len(try_me('ben')) != 0
--- yourpackage/lib.py
+++ yourpackage/lib.py
@@ -1,4 +1,2 @@
 def try_me(x):
   return "Hi, how are you " + x + "?"
-
-print(try_me("ben"))
